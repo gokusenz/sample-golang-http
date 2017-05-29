50de7fcbf9b8a708111a18fd30e528f1b14861b0
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-05-29 13:45:30 +0700

    change regex

--- src/regex.go
+++ src/regex.go
@@ -5,12 +5,16 @@ import (
 	"regexp"
 )
 
-func main() {
-	s := "Nobody expects the Spanish inquisition."
-	re1, _ := regexp.Compile(`(expects (...) Spanish)`)
-	result := re1.FindStringSubmatch(s)
+func regex(str string, pattern string) string {
+	// str := `100.100.100.100 - - [23/Feb/2015:03:03:56 +0100] "GET /folder/file.mp3 HTTP/1.1" 206 5637064 "-" "AppleCoreMedia/1.0.0.12B466 (iPhone; U; CPU OS 8_1_3 like Mac OS X; da_dk)"`
+	// r, _ := regexp.Compile(`([.0-9]+) .*?\[([0-9a-zA-Z:\/+ ]+)\].*?"[A-Z]+ \/([^\/ ]+)\/([a-zA-Z0-9\-.]+).*" ([0-9]{3}) .*"(.*?)"$`)
+	r, _ := regexp.Compile(pattern)
 
-	for k, v := range result {
-		fmt.Printf("%d. %s\n", k, v)
+	result := r.FindStringSubmatch(str)
+	fmt.Println(result)
+
+	for index, match := range result {
+		fmt.Printf("%d. %s\n", index, match)
 	}
+	return result[1]
 }
